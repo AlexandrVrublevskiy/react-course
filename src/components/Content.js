768c5154ebeb0820740289241c21915e54150664
Alex Vr <alexvruafm.gmail.com>
2020-08-21 17:31:29 +0300

    pre template

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Content.css'
+
+function Content(){
+    return (
+       
+        <div className=''>
+            <div className='blue'></div>
+        </div>
+    
+        
+    )
+}
+
+export default Content;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let NavBar = () => {
+    return (
+        <nav className='green'></nav>
+    )
+}
+
+export default NavBar;
